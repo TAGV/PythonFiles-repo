6ccf7d484c075850e2204b5c295d90ad6ada1719
TAGV <tanpvppcoe@gmail.com>
2021-10-07 13:41:27 +0530

    trick

--- PythonTricks/Trick_7.py
+++ PythonTricks/Trick_7.py
@@ -5,3 +5,10 @@ mylist = [1,2,3,4,5,6,23,45,12,89,100,3,4,5,23,45,35,101,'this','cat','is','mine
 list_no_duplicates = list(set(mylist))              #set is unique,hence all duplicate entries are removed
 
 print(list_no_duplicates)
+
+
+max_repeated = max(set(mylist),key=mylist.count)
+print(max_repeated)
+
+min_repeated = min(set(mylist),key=mylist.count)
+print(min_repeated)
