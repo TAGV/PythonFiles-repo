709a1975ff975f0f876b5be210c79393483d96f1
TAGV <tanpvppcoe@gmail.com>
2021-10-07 13:35:11 +0530

    trick

---
+++ PythonTricks/Trick_7.py
@@ -0,0 +1,7 @@
+#Remove the duplicates from the list
+
+mylist = [1,2,3,4,5,6,23,45,12,89,100,3,4,5,23,45,35,101,'this','cat','is','mine','this']
+
+list_no_duplicates = list(set(mylist))              #set is unique,hence all duplicate entries are removed
+
+print(list_no_duplicates)
