3d841f9719e495d10d40f14ca7c984e0f3604981
TAGV <tanpvppcoe@gmail.com>
2021-10-28 23:18:57 +0530

    usage of shebang operator

---
+++ pyscipt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+print("This is a sample python script to be run from command line!!!")
+print("Check the usage of shebang line at the top of the file")
+print("directly run this file as './pyscript' from terminal")
