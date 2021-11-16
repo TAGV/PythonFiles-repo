3d7649f0d6cf79550de633174a36208f12784382
TAGV <tanpvppcoe@gmail.com>
2021-11-16 17:35:43 +0530

    List Slicing - Shallow & Deep copying

---
+++ PythonTricks/Trick_25.py
@@ -0,0 +1,30 @@
+#List slicing & (shallow and deep copying the list)
+
+mlist = [1,2,3,4,5]
+
+# Deep copy of the list
+a = mlist
+
+print(mlist)
+
+#Creating a new list in place
+mlist[:] = [6,7,8,9]
+print(mlist)
+print(a)
+
+print(id(mlist))
+print(id(a))
+print(a is mlist)
+
+print("==================")
+nlist = [23,44,114]
+
+# Shallow copy of the list
+b = nlist[:]
+
+print(nlist)
+print(b)
+
+print(id(nlist))
+print(id(b))
+print(b is nlist)
