c2a00ed75a4d4f4e8b8e95395affc831ccd8d9b4
TAGV <tanpvppcoe@gmail.com>
2021-11-24 00:39:53 +0530

    opencv: image manipulation

--- Modules/opencv_demo.py
+++ Modules/opencv_demo.py
@@ -1,13 +1,29 @@
+import random
+
 import cv2
 
-img = cv2.imread('IMG.jpg',-1)
-img = cv2.resize(img,(400,400))
-img = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
+img = cv2.imread('IMG.jpg',1)
+img = cv2.resize(img,(700,800))
+#img = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
+print(type(img))
+#print(img)
+#print(img.shape) # 3*3 matrix of pixels(height,width,channels)=(rows,columns,color space(BGR))
+
+#Changing pixels in the image
+#Looping through first 100 rows of the image along the width
+for i in range(100):
+    for j in range(img.shape[1]):   #0:rows,1:coloumns,2:colors
+        img[i][j] = [random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+
+#Copy and paste parts of image
+tag = img[400:600,600:800]
+img[100:300,500:600] = tag
+
 cv2.imshow('image',img)
-cv2.waitKey(10000)
+cv2.waitKey(10000)  #Waiting for 10 secs
 cv2.destroyAllWindows()
 cv2.waitKey(1)              # Stack overflow soln to close the window
-#cv2.waitKey(1)
+cv2.waitKey(1)
 #cv2.waitKey(1)
 #cv2.waitKey(1)
 
