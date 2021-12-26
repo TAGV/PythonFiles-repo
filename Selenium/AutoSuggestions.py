40a4bcae80e4367ff8a342aaaf48f7f3aefa0a12
TAGV <tanpvppcoe@gmail.com>
2021-12-26 21:40:59 +0530

    Page Object Model Demo

--- Selenium/AutoSuggestions.py
+++ Selenium/AutoSuggestions.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.by import By
+from Locater_POM.Element_locaters import EleLocate      #Page Objects
 
 
 url = "https://www.redbus.in/"
@@ -11,9 +12,9 @@ driver = webdriver.Firefox()
 driver.get(url)
 driver.maximize_window()
 
-driver.find_element(By.XPATH,'//input[@id="src"]').send_keys('de')
+driver.find_element(By.XPATH,EleLocate.destination_box).send_keys('de')
 time.sleep(3)
-suggestion_list = driver.find_elements(By.XPATH,'//ul[@class="autoFill homeSearch"]//li')
+suggestion_list = driver.find_elements(By.XPATH,EleLocate.suggestions)
 print(len(suggestion_list))
 
 for sugg in suggestion_list:
---
+++ Selenium/Locater_POM/Element_locaters.py
@@ -0,0 +1,7 @@
+# This is demo for Page Object Model
+
+# used in files : Autosuggestions.py
+
+class EleLocate():
+    destination_box = '//input[@id="src"]'
+    suggestions = '//ul[@class="autoFill homeSearch"]//li'
