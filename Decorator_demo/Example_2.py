b0c4242508875d8d92bade5c358a924a4946ca75
TAGV <tanpvppcoe@gmail.com>
2021-12-28 00:18:26 +0530

    generators vs iterators

--- Decorator_demo/Example_2.py
+++ Decorator_demo/Example_2.py
@@ -10,8 +10,15 @@ def calculate_time(func):
 
 @calculate_time
 def calculate():
-    for it in range(100_000_000):
+    print("This is a generator")        #This is more efficient,incase of large range of data
+    for it in range(100_000_00):
         it*5
 
+@calculate_time
+def calculate2():
+    print("This is a iterator")
+    for it in list(range(100_000_00)):
+        it*5
 
 calculate()
+calculate2()
