f1689acc8f0aa3d25ae1840dac8c94c4010c2361
TAGV <tanpvppcoe@gmail.com>
2022-01-03 23:31:36 +0530

    Getting obj list

--- Classes_testdef/GettingAllClassInstances.py
+++ Classes_testdef/GettingAllClassInstances.py
@@ -5,6 +5,7 @@ class Items():
     def __init__(self,name,price):
         self.name = name
         self.price = price
+        assert self.price >= 100,"The price is not more than or equal to 100"
         Items.counter += 1
         Items.objlist.append(self)      # Since this will be called atleast once for every object created
 
@@ -17,9 +18,10 @@ class Items():
     def getObjList(self):
         return Items.objlist
 
-item1 = Items("Bag","100")
-item2 = Items("Table","200")
-item3 = Items("Chair","300")
+item1 = Items("Bag",100)
+item2 = Items("Table",200)
+item3 = Items("Chair",300)
+#item4 = Items("Pebble",50)  # This will cause assertion error
 
 print(item1)
 print(item2)
